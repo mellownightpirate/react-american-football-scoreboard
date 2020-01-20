7de79f5f0c5ab0d31d65414ea0189558ba1d1f4c
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-01-20 11:40:22 +0000

    Established application's state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [score, setScore] = useState();
+  const [home, setHome] = useState(0);
+  const [away, setAway] = useState(0);
 
   return (
     <div className="container">
